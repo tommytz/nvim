24122603157aa22a79e24931bac868b214b05d30
tommytz <ttardizuch@gmail.com>
2025-10-10 19:34:50 +1300

    added some more options to init.lua

--- init.lua
+++ init.lua
@@ -6,3 +6,12 @@ require("numbertoggle")
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+
+vim.opt.hlsearch = false
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.mouse = "nvi"
+vim.opt.signcolumn = "yes"
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.cmd [[colorscheme retrobox]]
