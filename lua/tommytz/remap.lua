90db460115d9485a36cd8aecbab0adc5d56997fd
tommytz <ttardizuch@gmail.com>
2025-10-12 22:53:24 +1300

    added fuzzy find keymaps

--- lua/tommytz/remap.lua
+++ lua/tommytz/remap.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Fuzzy find shortcuts
+vim.keymap.set("n", "<leader>ff", "<Cmd>:Fzf files<CR>")
+vim.keymap.set("n", "<leader>fg", "<Cmd>:Fzf grep<CR>")
