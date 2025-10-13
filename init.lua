48408f38f036a7b1aba2941782b758b948c2e64c
tommytz <ttardizuch@gmail.com>
2025-10-14 00:39:23 +1300

    added some more config options

--- init.lua
+++ init.lua
@@ -6,17 +6,23 @@ require("numbertoggle")
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.copyindent = true
 
+-- vim.opt.showmode = false -- Only enable this when you figure out how to show the mode in the status line
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.hlsearch = false
 vim.opt.smartcase = true
 vim.opt.ignorecase = true
-vim.opt.mouse = "nvi"
+vim.opt.incsearch = true
+vim.opt.mouse = "a"
 vim.opt.signcolumn = "yes"
 vim.opt.swapfile = false
 vim.opt.termguicolors = true
 vim.cmd("colorscheme retrobox")
+vim.opt.wrap = false
+vim.opt.confirm = true
 
 -- LSP
 vim.lsp.enable("luals")
