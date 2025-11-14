c826021701e410ac5442ae0eaf2657b548dd3043
tommytz <ttardizuch@gmail.com>
2025-11-14 17:07:06 +1300

    adds blinkcmp docs popups

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -35,7 +35,7 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+    completion = { documentation = { auto_show = true, auto_show_delay_ms = 500 } },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
